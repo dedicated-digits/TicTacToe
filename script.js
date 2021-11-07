165dd47cdbb14200f7b2778d543b98f6b6a6b321
Nandan Varma <74198723+nandan-varma@users.noreply.github.com>
2021-11-07 11:54:05 +0530

    update

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 var rows=0;
-var grid=[];
-var gridvalue=[];
-var form = document.getElementById("inputform");
-form.action = "main.html";
-var gameblock = document.getElementById("Game");
-function onSubmit(){
-    window.location.href("..\main.html");
-}
+var buttongrid
 function blocks(num){
+    alert("hello");
     var tempbtn;
+    const Gameblock = document.getElementById("Game");
     for (let index = 0; index < num; index++) {
-        tempbtn=document.createElement("button")
+        tempbtn=document.createElement("button");
         tempbtn.innerHTML = " ";
-        tempbtn.onclick = clickevent(this);
-        gameblock.appendChild(tempbtn);
-        grid.push(tempbtn);
-        gridvalue.push(" ");
+        Gameblock.appendChild(tempbtn);
+        
     }
 }
-function clickevent(e){
-    e.target.innerHTML = "X";
+function GameGrid(num){
     
+    for (let i = 0; i < num*num; i++) {
+        
+    }
+}
+function onSubmit(){
+    document.getElementById("Game").style.display="block";
+    document.getElementById("NameBlock").style.display="none";
+    const num = parseInt(document.getElementById("number").value);
+    GameGrid(num);
+}
+function ToMenu(){
+    document.getElementById("NameBlock").style.display="block";
+    document.getElementById("Game").style.display = "None";
 }
